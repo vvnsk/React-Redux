58409514557f37865ca0fc5dfda3323837eb8bb6
Saikrishna <vinnakotavnsaikrishna@gmail.com>
2019-06-16 17:27:38 -0500

    Central Redux Initial State

--- src/components/books/BooksPage.js
+++ src/components/books/BooksPage.js
@@ -1,41 +1,65 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as bookActions from "../../redux/actions/bookActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
+import BookList from "./BookList";
 
 class BooksPage extends React.Component {
   componentDidMount() {
-    this.props.actions.loadBooks().catch(error => {
-      alert("Loading courses failed" + error);
-    });
+    const { books, authors, actions } = this.props;
+
+    if (books.length === 0) {
+      actions.loadBooks().catch(error => {
+        alert("Loading books failed" + error);
+      });
+    }
+
+    if (authors.length === 0) {
+      actions.loadAuthors().catch(error => {
+        alert("Loading authors failed" + error);
+      });
+    }
   }
+
   render() {
     return (
       <>
         <h2>Books</h2>
-        {this.props.books.map(book => (
-          <div key={book.title}>{book.title}</div>
-        ))}
+        <BookList books={this.props.books} />
       </>
     );
   }
 }
 
 BooksPage.propTypes = {
+  authors: PropTypes.array.isRequired,
   books: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
   return {
-    books: state.books
+    books:
+      state.authors.length === 0
+        ? []
+        : state.books.map(book => {
+            return {
+              ...book,
+              authorName: state.authors.find(a => a.id === book.authorId).name
+            };
+          }),
+    authors: state.authors
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(bookActions, dispatch)
+    actions: {
+      loadBooks: bindActionCreators(bookActions.loadBooks, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }
   };
 }
 
--- src/redux/reducers/bookReducer.js
+++ src/redux/reducers/bookReducer.js
@@ -1,6 +1,7 @@
 import * as types from "../actions/actionTypes";
+import initialState from "./initialState";
 
-export default function bookReducer(state = [], action) {
+export default function bookReducer(state = initialState.books, action) {
   switch (action.type) {
     case types.CREATE_BOOK:
       return [...state, { ...action.book }];

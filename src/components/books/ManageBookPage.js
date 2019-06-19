bb9c538ffd90591138207f4a9fe6cceb20209006
Saikrishna <vinnakotavnsaikrishna@gmail.com>
2019-06-19 06:31:22 -0500

    Front end validation

--- src/components/books/ManageBookPage.js
+++ src/components/books/ManageBookPage.js
@@ -45,8 +45,22 @@ function ManageBookPage({
     }));
   }
 
+  function formIsValid() {
+    const { title, authorId, category } = book;
+    const errors = {};
+
+    if (!title) errors.title = "Title is required.";
+    if (!authorId) errors.author = "Author is required";
+    if (!category) errors.category = "Category is required";
+
+    setErrors(errors);
+    // Form is valid if the errors object still has no properties
+    return Object.keys(errors).length === 0;
+  }
+
   function handleSave(event) {
     event.preventDefault();
+    if (!formIsValid()) return;
     setSaving(true);
     saveBook(book)
       .then(() => {
